10d14150c5741c45fcd2d071bd4b406c840338f0
Vincent Ferraro <v.ferraro@live.fr>
2022-09-28 12:57:15 +0200

    second header h2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="div-app">
       <h1> Header added by test-branch</h1>
       <TodoList />
+      <h2> second header added by test-branch</h2>
     </div>
   );
 }
